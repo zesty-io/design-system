d2457124803624e7a840547383ed5f1965be795f
stuart runyan <shrunyan@gmail.com>
2018-07-23 17:27:36 -0700

    added: with loader component guide

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./views/app";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './views/app'
 
 ReactDOM.render(
   <App
     components={[
-      "Button",
-      "Button Group",
-      "Card",
-      "Divider",
-      "Loader",
-      "Search",
-      "Select",
-      "Input",
-      "Toggle",
-      "Infotip",
-      "TextFieldType",
-      "NumberFieldType",
-      "ColorFieldType",
-      "Nav"
+      'Button',
+      'Button Group',
+      'Card',
+      'Divider',
+      'Loader',
+      'WithLoader',
+      'Search',
+      'Select',
+      'Input',
+      'Toggle',
+      'Infotip',
+      'TextFieldType',
+      'NumberFieldType',
+      'ColorFieldType',
+      'Nav'
     ]}
   />,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
