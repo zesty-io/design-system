3a89dc06a47b7fc1148220b708ef28aef5533f83
stuart runyan <shrunyan@gmail.com>
2018-07-23 17:28:40 -0700

    fixed: show button in search

--- core/src/Search/Search.js
+++ core/src/Search/Search.js
@@ -1,33 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-// FIX this reference doesn't work
-// import Button from '../Button'
+import { Button } from '../Button'
 
-import styles from "./Search.less";
-import cx from "classnames";
+import styles from './Search.less'
+import cx from 'classnames'
 
 export class Search extends Component {
   state = {
-    searchTerm: ""
-  };
+    searchTerm: ''
+  }
   componentDidMount() {
     // lets the user override the initial value in the search box
     if (this.props.override) {
-      this.setState({ searchTerm: this.props.override });
+      this.setState({ searchTerm: this.props.override })
     }
   }
   render() {
     return (
       <form
         className={cx(styles.search, this.props.className)}
-        onSubmit={this.handleSubmit}
-      >
-        {/* <Button className={styles.searchBtn} type="submit">
+        onSubmit={this.handleSubmit}>
+        <Button className={styles.searchBtn} type="submit">
           <i
             className={cx(styles.searchIcon, 'fa fa-search')}
             aria-hidden="true"
           />
-        </Button> */}
+        </Button>
         <input
           type="text"
           name="term"
@@ -39,17 +37,17 @@ export class Search extends Component {
           onChange={this.handleKeyUp}
         />
       </form>
-    );
+    )
   }
   handleSubmit = evt => {
-    evt.preventDefault();
-    this.props.onSubmit(this.state.searchTerm);
-  };
+    evt.preventDefault()
+    this.props.onSubmit(this.state.searchTerm)
+  }
   handleKeyUp = evt => {
-    evt.preventDefault();
+    evt.preventDefault()
     // return the target value of the input
     this.setState({ searchTerm: evt.target.value }, () =>
       this.props.onKeyUp(this.state.searchTerm)
-    );
-  };
+    )
+  }
 }
