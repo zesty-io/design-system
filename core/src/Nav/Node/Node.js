506a1aff9ea896cc61ca8646b6741cc3d5d77e41
Grant Glidewell <grant.glidewell@zesty.io>
2018-07-30 11:26:23 -0700

    changes to Nav component to increase performance and usability

--- core/src/Nav/Node/Node.js
+++ core/src/Nav/Node/Node.js
@@ -1,37 +1,45 @@
-import React from "react";
-// import { Link } from "react-router-dom";
+import React, { PureComponent } from 'react'
+import styles from './Node.less'
 
-import styles from "./Node.less";
-export function Node(props) {
-  // style if a node is active
-  const isActive = (props.active && styles.active) || "";
-  // style is a node is selected
-  const isSelected = (props.selected === props.path && styles.selected) || "";
-  // check if a parent node is collapsed
-  const isClosed = props.closed && styles.closed;
-  let isCollapsed =
-    props.collapsed &&
-    props.collapsed.reduce((acc, collapsed) => {
-      if (collapsed === props.path) acc.push(collapsed);
-      return acc;
-    }, []).length;
-  return (
-    <li
-      className={`${styles.item} ${isActive} ${
-        styles[`depth${props.depth}`]
-      } ${isSelected} ${isClosed}`}
-      key={props.path}
-    >
-      <a href={`/${props.path}`}>
-        <i className={`fa fa-${props.icon}`} />
-        <span>{props.name}</span>
-      </a>
-      {props.children && (
-        <i
-          className={isCollapsed ? "fa fa-caret-left" : "fa fa-caret-down"}
-          onClick={() => props.handleOpen(props.path)}
-        />
-      )}
-    </li>
-  );
+export class Node extends PureComponent {
+  render() {
+    const {
+      active,
+      selected,
+      closed,
+      collapsed,
+      depth,
+      path,
+      icon,
+      name,
+      children,
+      handleOpen
+    } = this.props
+    // style if a node is active
+    const isActive = (active && styles.active) || ''
+    // style is a node is selected
+    const isSelected = (selected === path && styles.selected) || ''
+    // check if a parent node is collapsed
+    const isClosed = closed && styles.closed
+    const isCollapsed =
+      collapsed && collapsed.filter(collapsed => collapsed === path).length
+    return (
+      <li
+        className={`${styles.item} ${isActive} ${
+          styles[`depth${depth}`]
+        } ${isSelected} ${isClosed}`}
+        key={path}>
+        <a href={`/${path}`}>
+          <i className={`fa fa-${icon}`} />
+          <span>{name}</span>
+        </a>
+        {children && (
+          <i
+            className={isCollapsed ? 'fa fa-caret-left' : 'fa fa-caret-down'}
+            onClick={() => handleOpen(path)}
+          />
+        )}
+      </li>
+    )
+  }
 }
