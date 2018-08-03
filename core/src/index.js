8b766f1f4dfd13885c81e62a6bd37bf53ce125fa
Grant Glidewell <grant.glidewell@zesty.io>
2018-08-03 14:46:19 -0700

    added: fieldtypes to index export in core

--- core/src/index.js
+++ core/src/index.js
@@ -1,14 +1,29 @@
-export { Button } from "./Button";
-export { AppLink } from "./AppLink";
-export { ButtonGroup } from "./ButtonGroup";
-export { Card, CardHeader, CardContent, CardFooter } from "./Card";
-export { Divider } from "./Divider";
-export { Infotip } from "./Infotip";
-export { Input } from "./Input";
-export { Loader } from "./Loader";
-export { Search } from "./Search";
-export { Select, Option } from "./Select";
-export { Toggle } from "./Toggle";
-export { Url } from "./Url";
-export { WithLoader } from "./WithLoader";
-export { Nav } from "./Nav";
+export { Button } from './Button'
+export { AppLink } from './AppLink'
+export { ButtonGroup } from './ButtonGroup'
+export { Card, CardHeader, CardContent, CardFooter } from './Card'
+export { Divider } from './Divider'
+export { Infotip } from './Infotip'
+export { Input } from './Input'
+export { Loader } from './Loader'
+export { Search } from './Search'
+export { Select, Option } from './Select'
+export { Toggle } from './Toggle'
+export { Url } from './Url'
+export { WithLoader } from './WithLoader'
+export { Nav } from './Nav'
+export { WYSIWYGFieldType } from './WYSIWYGFieldType'
+export { UrlFieldType } from './UrlFieldType'
+export { TextFieldType } from './TextFieldType'
+export { TextareaFieldType } from './TextareaFieldType'
+export { QuillFieldType } from './QuillFieldType'
+export { OneToOneFieldType } from './OneToOneFieldType'
+export { NumberFieldType } from './NumberFieldType'
+export { DropDownFieldType } from './DropDownFieldType'
+export { DraftFieldType } from './DraftFieldType'
+export { DateTimeFieldType } from './DateTimeFieldType'
+export { DateFieldType } from './DateFieldType'
+export { CurrencyFieldType } from './CurrencyFieldType'
+export { ColorFieldType } from './ColorFieldType'
+export { CollapsibleCard } from './CollapsibleCard'
+export { BinaryFieldType } from './BinaryFieldType'
