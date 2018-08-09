e55adb06a1f40cb061acaf993c2b6b7e6c05b476
stuart runyan <shrunyan@gmail.com>
2018-08-08 17:21:52 -0700

    added: snapshot to button test

--- core/src/Button/Button.test.js
+++ core/src/Button/Button.test.js
@@ -4,12 +4,14 @@ import renderer from "react-test-renderer";
 
 import { Button } from "../../dist/Button";
 
-test("Button renders", () => {
-  // const el = React.createElement(Button);
-  // const str = renderToString(el);
-  // expect(str).toBe('<button class="button--qXZ5J" data-reactroot=""></button>');
-
+test("Button snapshot", () => {
   const component = renderer.create(<Button />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test("Button renders", () => {
+  const el = React.createElement(Button);
+  const str = renderToString(el);
+  expect(str).toBe('<button class="button--qXZ5J" data-reactroot=""></button>');
 });
