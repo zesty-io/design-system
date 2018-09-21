1de294042f0cb5d1c564423b99873df06f4c1ea1
Stuart Runyan <shrunyan@gmail.com>
2018-09-21 16:31:39 -0700

    fixed: date field. Now keeps date as string and instaniates moment object in prop

--- core/src/DateFieldType/DateFieldType.js
+++ core/src/DateFieldType/DateFieldType.js
@@ -12,30 +12,33 @@ export class DateFieldType extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: moment(props.value)
+      date: props.value
     };
   }
   onChange = m => {
-    if (this.props.onChange) {
-      this.props.onChange(
-        this.props.name,
-        m.clone().format(),
-        this.props.datatype
-      );
-    }
-    this.setState({ date: m });
+    /*
+    Outside of this component we don't know how this timestamp will be dealt
+    with so instead of emmiting a moment object we resolve a string and push
+    the moment instaniation to the `DatePicker` component
+    */
+    const dateStr = m.clone().format();
+    this.setState({ date: dateStr }, () => {
+      if (this.props.onChange) {
+        this.props.onChange(this.props.name, dateStr, this.props.datatype);
+      }
+    });
   };
   render() {
     return (
       <label className={styles.DateFieldType}>
-        <span className={styles.DateFieldTypeLabel}> {this.props.label}</span>
+        <span className={styles.DateFieldTypeLabel}>{this.props.label}</span>
         <span className={styles.DateFieldTypeInput}>
           <i className={cx(styles.Icon, "fa fa-calendar")} />
           <DatePicker
             {...this.props}
             className={cx(styles.DatePicker, this.props.className)}
             onChange={this.onChange}
-            selected={this.state.date}
+            selected={this.state.date ? moment(this.state.date) : null}
             yearDropdownItemNumber={15}
             dateFormat="LLL"
             peekNextMonth
