a18ad82c6bda464d35e58cf99ef5f75d901c687f
Stuart Runyan <shrunyan@gmail.com>
2018-09-21 14:25:12 -0700

    WIP: date picker

--- core/src/DateFieldType/DateFieldType.js
+++ core/src/DateFieldType/DateFieldType.js
@@ -2,48 +2,49 @@ import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import PropTypes from "prop-types";
 import moment from "moment";
+import cx from "classnames";
 
 import styles from "./DateFieldType.less";
-import "react-datepicker/dist/react-datepicker-cssmodules.css";
-
+// import "react-datepicker/dist/react-datepicker-cssmodules.css";
+import "react-datepicker/dist/react-datepicker.css";
 export class DateFieldType extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: moment()
+      date: moment(props.value)
     };
   }
-  componentDidMount() {
-    if (this.props.value) {
-      this.setState({
-        date: moment(this.props.value)
-      });
-    }
-  }
-  onChange = date => {
+  onChange = m => {
+    console.log("DateFieldType:onChange", m);
     if (this.props.onChange) {
       this.props.onChange(
         this.props.name,
-        moment(date).format(),
+        m.clone().format(),
         this.props.datatype
       );
     }
-    this.setState({
-      date: date
-    });
+    this.setState({ date: m });
   };
   render() {
     return (
-      <article className={styles.DateFieldType}>
-        <div className={styles.DateFieldTypeLabel}>
-          <label>{this.props.label}</label>
-        </div>
-        <DatePicker
-          {...this.props}
-          onChange={this.onChange}
-          selected={this.state.date}
-        />
-      </article>
+      <label className={styles.DateFieldType}>
+        <span className={styles.DateFieldTypeLabel}> {this.props.label}</span>
+        <span className={styles.DateFieldTypeInput}>
+          <i className={cx(styles.Icon, "fa fa-calendar")} />
+          <DatePicker
+            {...this.props}
+            className={cx(styles.DatePicker, this.props.className)}
+            onChange={this.onChange}
+            selected={this.state.date}
+            yearDropdownItemNumber={15}
+            dateFormat="LLL"
+            peekNextMonth
+            showMonthDropdown
+            showYearDropdown
+            dropdownMode="select"
+          />
+        </span>
+      </label>
     );
   }
 }
