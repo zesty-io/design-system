b8337527c38d0d48b6c640e6f9a4a9a5a0eecddc
Stuart Runyan <shrunyan@gmail.com>
2018-09-24 17:52:38 -0700

    fixed: ColorFieldType styling

--- core/src/ColorFieldType/ColorFieldType.js
+++ core/src/ColorFieldType/ColorFieldType.js
@@ -1,31 +1,28 @@
 import React, { Component } from "react";
-
+import cx from "classnames";
 import { Input } from "../Input";
-
 import styles from "./ColorFieldType.less";
+
 export class ColorFieldType extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      colorInput: "#ffffff",
-      required: props.required
+      value: props.value || "#ffffff"
     };
   }
-  componentDidMount() {
-    if (this.props.default) {
-      this.setState({ colorInput: this.props.default });
-    }
-  }
   render() {
-    const { colorInput } = this.state;
     return (
-      <article className={styles.ColorFieldType}>
-        <div className={styles.ColorFieldTypeLabel}>
-          <label>{this.props.label}</label>
+      <label className={styles.ColorFieldType}>
+        <span className={styles.ColorFieldTypeLabel}>{this.props.label}</span>
+        <div className={styles.ColorFieldTypeInput}>
+          <i className={cx(styles.Icon, "fa fa-paint-brush")} />
+          <Input
+            type="color"
+            onChange={this.onChange}
+            value={this.state.value}
+          />
         </div>
-        <Input type="text" onChange={this.onChange} value={colorInput} />
-        <Input type="color" onChange={this.onChange} value={colorInput} />
-      </article>
+      </label>
     );
   }
   onChange = evt => {
@@ -37,7 +34,7 @@ export class ColorFieldType extends Component {
       );
     }
     this.setState({
-      colorInput: evt.target.value
+      value: evt.target.value
     });
   };
 }
