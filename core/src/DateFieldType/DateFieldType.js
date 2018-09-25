e1b65813b736b7f010a4142d011780996bf4c8c7
Stuart Runyan <shrunyan@gmail.com>
2018-09-25 11:32:46 -0700

    changed: DateFieldType now uses the native browser date input

--- core/src/DateFieldType/DateFieldType.js
+++ core/src/DateFieldType/DateFieldType.js
@@ -1,32 +1,42 @@
 import React, { Component } from "react";
-import DatePicker from "react-datepicker";
+// import DatePicker from "react-datepicker";
 import PropTypes from "prop-types";
-import moment from "moment";
+// import moment from "moment";
 import cx from "classnames";
 
 import styles from "./DateFieldType.less";
-import "react-datepicker/dist/react-datepicker-cssmodules.css";
+// import "react-datepicker/dist/react-datepicker-cssmodules.css";
 // import "react-datepicker/dist/react-datepicker.css";
 
 export class DateFieldType extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      date: props.value
-    };
+    // this.state = {
+    //   date: props.value
+    // };
   }
-  onChange = m => {
+  onChange = evt => {
+    console.log("DateFieldType", evt.target.value);
+
+    if (this.props.onChange) {
+      this.props.onChange(
+        this.props.name,
+        evt.target.value,
+        this.props.datatype
+      );
+    }
+
     /*
     Outside of this component we don't know how this timestamp will be dealt
     with so instead of emmiting a moment object we resolve a string and push
     the moment instaniation to the `DatePicker` component
     */
-    const dateStr = m.clone().format();
-    this.setState({ date: dateStr }, () => {
-      if (this.props.onChange) {
-        this.props.onChange(this.props.name, dateStr, this.props.datatype);
-      }
-    });
+    // const dateStr = m.clone().format();
+    // this.setState({ date: dateStr }, () => {
+    //   if (this.props.onChange) {
+    //     this.props.onChange(this.props.name, dateStr, this.props.datatype);
+    //   }
+    // });
   };
   render() {
     return (
@@ -34,7 +44,13 @@ export class DateFieldType extends Component {
         <span className={styles.DateFieldTypeLabel}>{this.props.label}</span>
         <span className={styles.DateFieldTypeInput}>
           <i className={cx(styles.Icon, "fa fa-calendar")} />
-          <DatePicker
+          <input
+            className={cx(styles.DatePicker, this.props.className)}
+            type={this.props.type || "date"}
+            onChange={this.onChange}
+            value={this.props.value || ""}
+          />
+          {/*<DatePicker
             {...this.props}
             className={cx(styles.DatePicker, this.props.className)}
             onChange={this.onChange}
@@ -45,7 +61,7 @@ export class DateFieldType extends Component {
             showMonthDropdown
             showYearDropdown
             dropdownMode="select"
-          />
+          />*/}
         </span>
       </label>
     );
