2a43efe4d1a24930b6d98df537cf1f05e526ec89
Stuart Runyan <shrunyan@gmail.com>
2018-09-25 11:32:24 -0700

    added: UUIDFieldType for read only IDs

---
+++ core/src/UUIDFieldType/UUIDFieldType.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from "react";
+import cx from "classnames";
+import { Input } from "../Input";
+import styles from "./UUIDFieldType.less";
+
+export class UUIDFieldType extends PureComponent {
+  render() {
+    return (
+      <label className={styles.UUIDFieldType}>
+        <div className={styles.UUIDFieldTypeLabel}>
+          <span>{this.props.label}</span>
+        </div>
+        <div className={styles.DateFieldTypeInput}>
+          <i
+            className={cx(styles.Icon, "fa fa-clipboard")}
+            aria-hidden="true"
+          />
+          <Input
+            // {...this.props}
+            type="text"
+            readOnly={true}
+            defaultValue={this.props.value || "test"}
+          />
+        </div>
+      </label>
+    );
+  }
+}
---
+++ core/src/UUIDFieldType/index.js
@@ -0,0 +1,1 @@
+export { UUIDFieldType } from "./UUIDFieldType";
