c9932c974c7d4d2137d62f00600993dff60437a2
Grant Glidewell <grant.glidewell@zesty.io>
2018-10-12 11:45:48 -0700

    added: custom buttongroup to allow for more flexible applications

--- app/guides/ConfirmDialogGuide.js
+++ app/guides/ConfirmDialogGuide.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 import { Button } from '@zesty-io/core/dist/Button'
+import { ButtonGroup } from '@zesty-io/core/dist/ButtonGroup'
 import { CodeCard } from '../components/CodeCard'
 import { ConfirmDialog } from '@zesty-io/core/dist/ConfirmDialog'
 
 export class ConfirmDialogGuide extends Component {
   state = {
-    isOpen: false
+    isOpen: false,
+    customIsOpen: false
   }
   render() {
     return (
-      <React.Fragment>
+      <Fragment>
         <p>description</p>
         <Button onClick={() => this.setState({ isOpen: true })}>
-          Open Confirm
+          Open Confim
+        </Button>
+        <Button onClick={() => this.setState({ customIsOpen: true })}>
+          Open Confirm with Custom ButtonGroup
         </Button>
         <ConfirmDialog
           isOpen={this.state.isOpen}
@@ -23,15 +28,116 @@ export class ConfirmDialogGuide extends Component {
             console.log(response)
           }}
         />
+        <ConfirmDialog
+          isOpen={this.state.customIsOpen}
+          prompt="Custom button group">
+          <Button
+            id="confirmTrue"
+            kind="save"
+            onClick={() => {
+              this.setState({ customIsOpen: false })
+            }}>
+            {'Save'}
+          </Button>
+          <Button
+            id="confirmFalse"
+            kind="warn"
+            onClick={() => {
+              this.setState({ customIsOpen: false })
+            }}
+            text="Discard"
+          />
+        </ConfirmDialog>
 
-        <CodeCard header="Usage" height={250} open>
-          // usage guide here
-        </CodeCard>
+        <CodeCard
+          header="Usage"
+          height={250}
+          open
+          children={`<ConfirmDialog
+  isOpen={this.state.isOpen}
+  prompt="Is it safe to go alone?"
+  callback={response => {
+    this.setState({ isOpen: false })
+    console.log(response)
+  }}
+/>
+<ConfirmDialog
+  isOpen={this.state.customIsOpen}
+  prompt="Custom button group">
+  <Button
+    id="confirmTrue"
+    kind="save"
+    onClick={() => {
+      this.setState({ customIsOpen: false })
+    }}>
+    {'Save'}
+  </Button>
+  <Button
+    id="confirmFalse"
+    onClick={() => {
+      this.setState({ customIsOpen: false })
+    }}
+    text="Discard"
+  />
+</ConfirmDialog>`}
+        />
 
-        <CodeCard header="Code" height={250}>
-          // component code here
-        </CodeCard>
-      </React.Fragment>
+        <CodeCard
+          header="Code"
+          height={250}
+          children={`export const ConfirmDialog = props => {
+  return (
+    (props.isOpen && (
+      <section className={styles.confirmWrapper}>
+        <section className={cx(styles.Confirm, styles[props.kind])}>
+          <h1>{props.prompt}</h1>
+          <footer>
+            {props.children ? (
+              // custom buttonGroup renders
+              <ButtonGroup
+                className={'$/{styles.buttons} $/{
+            props.single ? styles.short : ''
+          }'}>
+                {props.children}
+              </ButtonGroup>
+            ) : (
+              <ButtonGroup
+                className={'$/{styles.buttons} $/{
+            props.single ? styles.short : ''
+          }'}>
+                <Button
+                  id="confirmTrue"
+                  type={props.kind}
+                  onClick={() => {
+                    props.callback(true)
+                  }}>
+                  {props.kind === 'warn' && (
+                    <i
+                      className="fa fa-exclamation-triangle"
+                      aria-hidden="true"
+                    />
+                  )}
+                  {props.confirmText || 'Yes'}
+                </Button>
+                <Button
+                  id="confirmFalse"
+                  onClick={() => {
+                    props.callback(false)
+                  }}
+                  className={props.single ? styles.hide : ''}
+                  text="No"
+                />
+              </ButtonGroup>
+            )}
+          </footer>
+        </section>
+      </section>
+    )) ||
+    null
+  )
+}`}
+        />
+      </Fragment>
     )
   }
 }
--- core/src/ConfirmDialog/ConfirmDialog.js
+++ core/src/ConfirmDialog/ConfirmDialog.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Fragment } from "react";
 import cx from "classnames";
 import { Button } from "../Button";
 import { ButtonGroup } from "../ButtonGroup";
@@ -12,35 +12,46 @@ export const ConfirmDialog = props => {
         <section className={cx(styles.Confirm, styles[props.kind])}>
           <h1>{props.prompt}</h1>
           <footer>
-            <ButtonGroup
-              className={`${styles.buttons} ${
-                props.single ? styles.short : ""
-              }`}
-            >
-              <Button
-                id="confirmTrue"
-                type={props.kind}
-                onClick={() => {
-                  props.callback(true);
-                }}
+            {props.children ? (
+              // custom buttonGroup renders
+              <ButtonGroup
+                className={`${styles.buttons} ${
+                  props.single ? styles.short : ""
+                }`}
               >
-                {props.kind === "warn" && (
-                  <i
-                    className="fa fa-exclamation-triangle"
-                    aria-hidden="true"
-                  />
-                )}
-                {props.confirmText || "Yes"}
-              </Button>
-              <Button
-                id="confirmFalse"
-                onClick={() => {
-                  props.callback(false);
-                }}
-                className={props.single ? styles.hide : ""}
-                text="No"
-              />
-            </ButtonGroup>
+                {props.children}
+              </ButtonGroup>
+            ) : (
+              <ButtonGroup
+                className={`${styles.buttons} ${
+                  props.single ? styles.short : ""
+                }`}
+              >
+                <Button
+                  id="confirmTrue"
+                  type={props.kind}
+                  onClick={() => {
+                    props.callback(true);
+                  }}
+                >
+                  {props.kind === "warn" && (
+                    <i
+                      className="fa fa-exclamation-triangle"
+                      aria-hidden="true"
+                    />
+                  )}
+                  {props.confirmText || "Yes"}
+                </Button>
+                <Button
+                  id="confirmFalse"
+                  onClick={() => {
+                    props.callback(false);
+                  }}
+                  className={props.single ? styles.hide : ""}
+                  text="No"
+                />
+              </ButtonGroup>
+            )}
           </footer>
         </section>
       </section>
