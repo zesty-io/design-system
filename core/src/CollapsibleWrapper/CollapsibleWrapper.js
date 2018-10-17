84a627ead2df071f8a8da2a3a9e6c58b46156ff1
Grant Glidewell <grant.glidewell@zesty.io>
2018-10-17 09:55:15 -0700

    added: collapsible wrapper

---
+++ core/src/CollapsibleWrapper/CollapsibleWrapper.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import styles from "./CollapsibleWrapper.less";
+
+export class CollapsibleWrapper extends Component {
+  state = {
+    collapsed: false
+  };
+  render() {
+    const { direction, children } = this.props;
+    const { collapsed } = this.state;
+    return (
+      <div
+        className={`${styles.CollapseRight} ${
+          collapsed ? styles.collapsed : ""
+        }`}
+      >
+        <i
+          className={
+            (collapsed ? "fa fa-bars " : "fa fa-times ") + styles.close
+          }
+          onClick={() => this.setState({ collapsed: !collapsed })}
+        />
+        {children}
+      </div>
+    );
+  }
+}
