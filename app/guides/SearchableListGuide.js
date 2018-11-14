a2f6a32a8e1382e15ebaa6e6db2575de152a8468
Grant Glidewell <grant.glidewell@zesty.io>
2018-11-14 09:09:57 -0800

    added: progress on functionality of searchable list

--- app/guides/SearchableListGuide.js
+++ app/guides/SearchableListGuide.js
@@ -5,30 +5,59 @@ import { CollapsibleCard } from '@zesty-io/core/dist/CollapsibleCard'
 import GithubEmbed from '../components/githubembed'
 
 export class SearchableListGuide extends Component {
+  state = { returnedData: [] }
+  componentDidMount() {
+    // spoof data
+    const randomGen = opt =>
+      Math.random()
+        .toString(opt)
+        .substring(7)
+    const ZUIDs = new Array(500).fill('').map(i => randomGen())
+    const values = new Array(500).fill('').map(i => randomGen(36))
+    const spoofedData = ZUIDs.reduce((acc, el, i) => {
+      acc[el] = { html: values[i], value: el }
+      return acc
+    }, {})
+    this.setState({ spoofedData })
+  }
+  handleSelect = (name, value, datatype) => {
+    console.log(name, value, datatype)
+  }
+  handleSearch = term => {
+    console.log(term)
+    if (term.length >= 3) {
+      const returnedData = Object.keys(this.state.spoofedData)
+        .filter(ZUID => {
+          return (
+            this.state.spoofedData[ZUID].html.includes(term) ||
+            this.state.spoofedData[ZUID].value.includes(term)
+          )
+        })
+        .map(matchedZUID => this.state.spoofedData[matchedZUID])
+      console.log(returnedData)
+      this.setState({ returnedData })
+    }
+  }
   render() {
+    const { returnedData } = this.state
     return (
       <React.Fragment>
         <p>Searchable List component</p>
         <p>Props: options, label</p>
         <br />
-        <SearchableList label="Label Field">
-          <Option text="option 1" value="value 1" />
-          <Option text="test 2" value="test 2" />
-          <Option text="able 3" value="able 3" />
-          <Option text="find 4" value="find 4" />
+        <SearchableList
+          label="Label Field"
+          name="searchableList"
+          datatype="dataSearchableList"
+          onSelect={this.handleSelect}
+          onSearch={this.handleSearch}>
+          {returnedData.map((el, i) => (
+            <Option key={i} value={el.value} text={el.html} />
+          ))}
         </SearchableList>
         <br />
         <CollapsibleCard header="Usage" open>
-          <GithubEmbed
-            height="300px"
-            code={`
-<SearchableList label="Label Field">
-  <Option text="option 1" value="value 1" />
-  <Option text="option 2" value="value 2" />
-  <Option text="option 3" value="value 3" />
-  <Option text="option 4" value="value 4" />
-</SearchableList>`}
-          />
+          <GithubEmbed height="300px" code={``} />
         </CollapsibleCard>
         <CollapsibleCard header="Code" collapsed>
           <GithubEmbed url="https://gist.githubusercontent.com/grantglidewell/58a6f8bbb0f89f77bef63a745b1e9570/raw/8a023d7a27f3a58fd75132437cc002c4ba054f90/DropDownFieldType.js" />
@@ -37,3 +66,7 @@ export class SearchableListGuide extends Component {
     )
   }
 }
+
+// return searched data after three characters are returned
+
+// onselect needs to return only a zuid
