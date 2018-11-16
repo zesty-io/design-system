572a03725eb740316d386961ad230935bc1478b1
Grant Glidewell <grant.glidewell@zesty.io>
2018-11-16 15:00:55 -0800

    added: charCount to textareaFieldType

--- core/src/TextareaFieldType/TextareaFieldType.js
+++ core/src/TextareaFieldType/TextareaFieldType.js
@@ -33,16 +33,16 @@ export class TextareaFieldType extends Component {
     return (
       <article
         className={`${styles.TextareaFieldType} ${
-          this.props.maxLength && this.state.value.length > this.props.maxLength
+          this.props.charCount && this.state.value.length > this.props.charCount
             ? styles.Error
             : ""
         }`}
       >
         <div className={styles.TextareaFieldTypeLabel}>
           <label>{this.props.label}</label>
-          {this.props.maxLength && (
+          {this.props.charCount && (
             <span>
-              {this.state.value.length}/{this.props.maxLength}
+              {this.state.value.length}/{this.props.charCount}
             </span>
           )}
         </div>
