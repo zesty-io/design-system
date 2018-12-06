4d36ba6fc0679426d14eaec51c65858139cd0178
Stuart Runyan <shrunyan@gmail.com>
2018-12-05 16:38:03 -0800

    fixed: refactor url field type to work with new standard props

--- core/src/UrlFieldType/UrlFieldType.js
+++ core/src/UrlFieldType/UrlFieldType.js
@@ -1,41 +1,11 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
+import cx from "classnames";
 
 import { Input } from "../Input";
 
 import styles from "./UrlFieldType.less";
-export class UrlFieldType extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      textInput: this.props.default || ""
-    };
-  }
-  render() {
-    const { textInput } = this.state;
-    return (
-      <article
-        className={`${styles.UrlFieldType} ${
-          this.props.charCount && textInput.length > this.props.charCount
-            ? styles.Error
-            : ""
-        }`}
-      >
-        <div className={styles.UrlFieldTypeLabel}>
-          <label>{this.props.label}</label>
-          {this.props.charCount && (
-            <span>
-              {textInput.length}/{this.props.charCount}
-            </span>
-          )}
-        </div>
-        <Input type="url" onChange={this.onChange} value={textInput} />
-      </article>
-    );
-  }
+export class UrlFieldType extends PureComponent {
   onChange = evt => {
-    this.setState({
-      textInput: evt.target.value
-    });
     if (this.props.onChange) {
       this.props.onChange(
         this.props.name,
@@ -44,4 +14,27 @@ export class UrlFieldType extends Component {
       );
     }
   };
+
+  render() {
+    return (
+      <article
+        className={cx(
+          styles.UrlFieldType,
+          this.props.maxLength && this.props.value.length > this.props.maxLength
+            ? styles.Error
+            : null
+        )}
+      >
+        <div className={styles.UrlFieldTypeLabel}>
+          <label>{this.props.label}</label>
+          {this.props.maxLength && (
+            <span>
+              {this.props.value.length}/{this.props.maxLength}
+            </span>
+          )}
+        </div>
+        <Input type="url" onChange={this.onChange} value={this.props.value} />
+      </article>
+    );
+  }
 }
