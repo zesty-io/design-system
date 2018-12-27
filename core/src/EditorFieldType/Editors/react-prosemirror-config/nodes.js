207f015bd07713e34673a5329c9609e45b843158
Stuart Runyan <shrunyan@gmail.com>
2018-12-27 12:29:47 -0800

    fixed: allow inline styles on images

--- core/src/EditorFieldType/Editors/react-prosemirror-config/nodes.js
+++ core/src/EditorFieldType/Editors/react-prosemirror-config/nodes.js
@@ -3,7 +3,10 @@ import { orderedList, bulletList, listItem } from "prosemirror-schema-list";
 import { tableNodes } from "prosemirror-tables";
 import { footnoteNodes } from "@aeaton/prosemirror-footnotes";
 
-// Takes a NodeList
+/**
+ * getElementAttrs
+ * attrs [NodeList]
+ */
 function getElementAttrs(attrs) {
   let newAttrs = {};
 
@@ -44,6 +47,7 @@ const images = {
       align: { default: null },
       width: { default: null },
       height: { default: null },
+      style: { default: null },
       "data-zuid": { default: null }
     },
     group: "inline",
@@ -54,14 +58,7 @@ const images = {
         priority: 51, // must be higher than the default image spec
         tag: "img[src]",
         getAttrs(dom) {
-          return {
-            src: dom.getAttribute("src"),
-            title: dom.getAttribute("title"),
-            alt: dom.getAttribute("alt"),
-            align: dom.getAttribute("align"),
-            width: dom.getAttribute("width"),
-            height: dom.getAttribute("height")
-          };
+          return getElementAttrs(dom.attributes);
         }
       }
     ],
