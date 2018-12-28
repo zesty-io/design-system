6e58e2de121fee53566288df16b8389afeb53469
Stuart Runyan <shrunyan@gmail.com>
2018-12-28 11:18:15 -0800

    added: support for attributes on paragraphs and headers

--- core/src/EditorFieldType/Editors/react-prosemirror-config/nodes.js
+++ core/src/EditorFieldType/Editors/react-prosemirror-config/nodes.js
@@ -115,6 +115,94 @@ const iframe = {
   ]
 };
 
+// :: NodeSpec A plain paragraph textblock. Represented in the DOM
+// as a `<p>` element.
+const paragraph = {
+  content: "inline*",
+  group: "block",
+  attrs: {
+    id: { default: null },
+    class: { default: null },
+    style: { default: null }
+  },
+  parseDOM: [
+    {
+      tag: "p",
+      getAttrs(dom) {
+        return getElementAttrs(dom.attributes);
+      }
+    }
+  ],
+  toDOM(node) {
+    return ["p", node.attrs, 0];
+  }
+};
+
+// :: NodeSpec A heading textblock, with a `level` attribute that
+// should hold the number 1 to 6. Parsed and serialized as `<h1>` to
+// `<h6>` elements.
+const heading = {
+  attrs: {
+    level: { default: 1 },
+    id: { default: null },
+    class: { default: null },
+    style: { default: null }
+  },
+  content: "inline*",
+  group: "block",
+  defining: true,
+  parseDOM: [
+    {
+      tag: "h1",
+      getAttrs(dom) {
+        const attrs = getElementAttrs(dom.attributes);
+        return { ...attrs, level: 1 };
+      }
+    },
+    {
+      tag: "h2",
+      getAttrs(dom) {
+        const attrs = getElementAttrs(dom.attributes);
+
+        console.log("heading2:parseDOM", attrs);
+
+        return { ...attrs, level: 2 };
+      }
+    },
+    {
+      tag: "h3",
+      getAttrs(dom) {
+        const attrs = getElementAttrs(dom.attributes);
+        return { ...attrs, level: 3 };
+      }
+    },
+    {
+      tag: "h4",
+      getAttrs(dom) {
+        const attrs = getElementAttrs(dom.attributes);
+        return { ...attrs, level: 4 };
+      }
+    },
+    {
+      tag: "h5",
+      getAttrs(dom) {
+        const attrs = getElementAttrs(dom.attributes);
+        return { ...attrs, level: 5 };
+      }
+    },
+    {
+      tag: "h6",
+      getAttrs(dom) {
+        const attrs = getElementAttrs(dom.attributes);
+        return { ...attrs, level: 6 };
+      }
+    }
+  ],
+  toDOM(node) {
+    return ["h" + node.attrs.level, { ...node.attrs, level: null }, 0];
+  }
+};
+
 export default {
   ...nodes,
   ...listNodes,
@@ -123,7 +211,13 @@ export default {
     cellContent: "block+"
   }),
   ...footnoteNodes,
-  ...images, // Custom image schema
+
+  // Custom nodes
   script,
-  iframe
+  iframe,
+
+  //override defaults
+  ...images,
+  heading,
+  paragraph
 };
