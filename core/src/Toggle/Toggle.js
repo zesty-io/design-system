08418755b3d4a8d8114252fd10ac2cd22ca8ee96
Grant Glidewell <grant.glidewell@zesty.io>
2019-01-02 16:22:23 -0800

    fixed: toggle checking issue

--- core/src/Toggle/Toggle.js
+++ core/src/Toggle/Toggle.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 import styles from "./Toggle.less";
-export function Toggle(props) {
+export function Toggle({ checked, onClick }) {
+  console.log("checked", checked);
   return (
     <label
       className={styles.switch}
       onClick={evt => {
         evt.stopPropagation();
         evt.preventDefault();
-        return props.onClick();
+        return onClick();
       }}
     >
-      <input type="checkbox" checked={props.checked ? "checked" : null} />
+      <input type="checkbox" checked={checked === 1} />
       <span className={styles.slider} />
     </label>
   );
