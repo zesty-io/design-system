bf66d587e5918276f9234fe69b76e1aca77064ba
Stuart Runyan <shrunyan@gmail.com>
2019-01-11 15:20:20 -0800

    fixed: do not load script tag code preventing document changes

--- core/src/EditorFieldType/Editors/react-prosemirror-config/nodes/script.js
+++ core/src/EditorFieldType/Editors/react-prosemirror-config/nodes/script.js
@@ -16,6 +16,25 @@ export const script = {
     }
   ],
   toDOM(node) {
-    return ["script", node.attrs];
+    /**
+    In order to prevent insertion of a script tag view dom methods, which
+    causes the loading to the javascript from the script tag src. We create
+    a temp element which we then pull the `string` of innerHTML and set a wrapping
+    elements innerHTML.
+
+    @see https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML#Security_considerations
+    **/
+    const script = document.createElement("script");
+    script.src = node.attrs.src;
+    script.type = node.attrs.type;
+    script.async = node.attrs.async;
+
+    const temp = document.createElement("span");
+    temp.appendChild(script);
+
+    const wrap = document.createElement("span");
+    wrap.innerHTML = temp.innerHTML;
+
+    return wrap;
   }
 };
