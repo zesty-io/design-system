7f1a0691aa5a8f83afe11dc0bf659c05a5dc0ce1
Grant Glidewell <grant.glidewell@zesty.io>
2019-01-23 13:55:04 -0800

    added:4px spacing for label

--- core/src/Label/Label.js
+++ core/src/Label/Label.js
@@ -6,7 +6,8 @@ export const Label = ({ label, name, datatype, required }) => (
   <label className={styles.Label}>
     {label}{" "}
     <span className={styles.Context}>
-      [{name}]{datatype}
+      <span>[{name}]</span>
+      <span>{datatype}</span>
     </span>
     {required && <span style={{ color: "#9a2803" }}>*</span>}
   </label>
