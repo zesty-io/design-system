29af05eb71b90b654001cea559c0624f2f352dcb
Stuart Runyan <shrunyan@gmail.com>
2019-02-01 13:23:25 -0800

    updated select to allow controlling filtering and loading state

--- core/src/InternalLinkFieldType/InternalLinkFieldType.js
+++ core/src/InternalLinkFieldType/InternalLinkFieldType.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import debounce from "lodash.debounce";
 
-import { SearchableList } from "../SearchableList";
-import { Option } from "../Select";
+import { Select, Option } from "../Select";
 
 import styles from "./InternalLinkFieldType.less";
-
 export class InternalLinkFieldType extends Component {
+  state = {
+    loading: false
+  };
+
+  onSearch = debounce((name, term, datatype) => {
+    if (term) {
+      this.setState({
+        loading: true
+      });
+      this.props.onSearch(term).then(() => {
+        this.setState({
+          loading: false
+        });
+      });
+    }
+  }, 250);
+
   render() {
     return (
       <article className={this.props.className}>
@@ -16,22 +32,24 @@ export class InternalLinkFieldType extends Component {
           </label>
         </div>
 
-        <SearchableList
+        <Select
           name={this.props.name}
           placeholder={this.props.placeholder}
+          value={this.props.value || "0"}
           onSelect={this.props.onChange}
-          onSearch={this.props.onSearch}
-          value={this.props.value}
+          onFilter={this.onSearch}
+          // always render search input
+          searchPlaceholder="Do not see the item you are looking for? Enter a term to search the API."
+          searchLength="0"
+          loading={this.state.loading}
         >
-          {/* You should always be able to un-link an internal link */}
+          {/* You should always be able to unlink an internal link */}
           <Option value="0" text="— None —" />
 
-          {this.props.children
-            ? this.props.children
-            : this.props.options.map((opt, i) => {
-                return <Option key={i} {...opt} />;
-              })}
-        </SearchableList>
+          {this.props.options.map((option, i) => {
+            return <Option key={i} {...option} />;
+          })}
+        </Select>
       </article>
     );
   }
