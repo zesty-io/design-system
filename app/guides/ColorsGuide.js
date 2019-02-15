c460fea96c7e8658aae5014cc6bfae3a0624350f
Grant Glidewell <grant.glidewell@zesty.io>
2019-02-15 09:19:53 -0800

    wip

---
+++ app/guides/ColorsGuide.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class ColorsGuide extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Zesty Colors</h2>
+        <img src="../assets/Colors.png" />
+      </React.Fragment>
+    )
+  }
+}
---
+++ app/guides/TypographyGuide.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TypographyGuide extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Zesty Typography</h2>
+        <img src="../assets/Typography.png" />
+        <img src="../assets/Typography-reversed.png" />
+      </React.Fragment>
+    )
+  }
+}
