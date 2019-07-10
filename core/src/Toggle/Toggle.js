6953abe0c6f555d7444eff5e61074109b602f0d6
Stuart Runyan <shrunyan@gmail.com>
2019-07-10 12:43:51 -0700

    fixed: Toggle component default value

--- core/src/Toggle/Toggle.js
+++ core/src/Toggle/Toggle.js
@@ -1,17 +1,14 @@
 import React from "react";
-
 import styles from "./Toggle.less";
-export function Toggle({ checked, onClick }) {
+
+/**
+  @props checked 0:1
+  @props onChange() React requires onChange handlers 
+**/
+export function Toggle({ checked, onChange }) {
   return (
-    <label
-      className={styles.switch}
-      onClick={evt => {
-        evt.stopPropagation();
-        evt.preventDefault();
-        return onClick();
-      }}
-    >
-      <input type="checkbox" defaultChecked={checked === 1} />
+    <label className={styles.switch}>
+      <input type="checkbox" checked={checked === 1} onChange={onChange} />
       <span className={styles.slider} />
     </label>
   );
