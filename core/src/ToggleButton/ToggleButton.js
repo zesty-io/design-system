d0e3f44faf2dab83f83291912fbf323942919a4e
Stuart Runyan <shrunyan@gmail.com>
2019-07-10 12:43:24 -0700

    convert ToggleButton to function component

--- core/src/ToggleButton/ToggleButton.js
+++ core/src/ToggleButton/ToggleButton.js
@@ -1,48 +1,40 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import cx from "classnames";
-
 import styles from "./ToggleButton.less";
-export class ToggleButton extends PureComponent {
-  onClick = evt => {
-    evt.stopPropagation();
-    evt.preventDefault();
 
-    this.props.onChange(
-      this.props.name,
-      this.props.value == 1 ? 0 : 1,
-      this.props.datatype
-    );
-  };
+/**
+  @props props.value 0:No 1:Yes
+**/
+export function ToggleButton(props) {
+  return (
+    <button
+      className={styles.ToggleButton}
+      onClick={evt => {
+        evt.stopPropagation();
+        evt.preventDefault();
 
-  /**
-    @props this.props.value 0:No 1:Yes
-  **/
-  render() {
-    return (
-      <button
-        className={styles.ToggleButton}
-        onClick={this.onClick}
-        disabled={this.props.disabled}
+        props.onChange(props.name, props.value == 1 ? 0 : 1, props.datatype);
+      }}
+      disabled={props.disabled}
+    >
+      <span
+        className={cx(
+          styles.default,
+          styles.off,
+          props.value == 0 ? styles.Selected : ""
+        )}
       >
-        <span
-          className={cx(
-            styles.default,
-            styles.off,
-            this.props.value == 0 ? styles.Selected : ""
-          )}
-        >
-          {this.props.offValue || "Off"}
-        </span>
-        <span
-          className={cx(
-            styles.default,
-            styles.on,
-            this.props.value == 1 ? styles.Selected : ""
-          )}
-        >
-          {this.props.onValue || "On"}
-        </span>
-      </button>
-    );
-  }
+        {props.offValue || "Off"}
+      </span>
+      <span
+        className={cx(
+          styles.default,
+          styles.on,
+          props.value == 1 ? styles.Selected : ""
+        )}
+      >
+        {props.onValue || "On"}
+      </span>
+    </button>
+  );
 }
