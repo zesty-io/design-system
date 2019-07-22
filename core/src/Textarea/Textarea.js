edb8c63f6f371ac41bc6d707e5878241d3ebffba
Stuart Runyan <shrunyan@gmail.com>
2019-07-22 13:39:51 -0700

    fixed: guard aginast react error on null values being set for textareas

--- core/src/Textarea/Textarea.js
+++ core/src/Textarea/Textarea.js
@@ -3,15 +3,11 @@ import styles from "./Textarea.less";
 import cx from "classnames";
 
 export function Textarea(props) {
-  // const opts = {};
-  // if (props.disabled) {
-  //   opts["disabled"] = "disabled";
-  // }
   return (
     <textarea
       {...props}
-      // {...opts}
       className={cx(styles.Textarea, props.className)}
+      value={this.props.value === null ? "" : this.props.value}
     />
   );
 }
