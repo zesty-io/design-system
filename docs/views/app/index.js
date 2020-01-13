d72e419e74a73305578a255ebe715477b40b834f
Stuart Runyan <shrunyan@gmail.com>
2020-01-13 13:53:24 -0800

    udpated: use hash router for docs site

--- docs/views/app/index.js
+++ docs/views/app/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import React from 'react'
+import { HashRouter, Route, Switch, Link } from 'react-router-dom'
 
 import { Contribute } from './Contribute'
 import * as Guides from '../../guides'
@@ -8,80 +8,71 @@ import { Nav } from '../../../core/src/Nav'
 
 import '@zesty-io/core/vendor.css'
 import styles from './app.less'
+export default function App(props) {
+  const selected = `/design-system#/${window.location.pathname.split('/')[1]}`
+  const getPath = el =>
+    `/design-system#/${el.toLowerCase().replace(' ', '-')}guide`
 
-export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter basename={'/design-system'}>
-        <Route
-          path="/"
-          render={props => {
-            return (
-              <section className={styles.App}>
-                <header className={styles.AppHeader}>
-                  <Link className={styles.Home} to="/">
-                    <i className={`fa fa-home ${styles.link}`} />{' '}
-                  </Link>
-                  <h1>Zesty.io Component Library</h1>
-                </header>
-                <section className={styles.AppWrap}>
-                  <nav className={styles.AppMenu}>
-                    <div className={styles.Title}>ATOMS</div>
-                    <Nav
-                      selected={'/' + props.location.pathname.split('/')[1]}
-                      tree={this.props.atoms.map(el => {
-                        return {
-                          label: el,
-                          path:
-                            '/' + el.toLowerCase().replace(' ', '-') + 'guide',
-                          icon: 'cube'
-                        }
-                      })}
-                    />
-                    <div className={styles.Title}>MOLECULES</div>
-                    <Nav
-                      selected={'/' + props.location.pathname.split('/')[1]}
-                      tree={this.props.molecules.map(el => {
-                        return {
-                          label: el,
-                          path:
-                            '/' + el.toLowerCase().replace(' ', '-') + 'guide',
-                          icon: 'cogs'
-                        }
-                      })}
-                    />
-                    <div className={styles.Title}>ORGANISMS</div>
-                    <Nav
-                      selected={'/' + props.location.pathname.split('/')[1]}
-                      tree={this.props.organisms.map(el => {
-                        return {
-                          label: el,
-                          path:
-                            '/' + el.toLowerCase().replace(' ', '-') + 'guide',
-                          icon: 'fa-snowflake-o'
-                        }
-                      })}
-                    />
-                  </nav>
-                  <main className={styles.AppShowcase}>
-                    <Switch>
-                      {/* Render routes for each component */}
-                      {Object.keys(Guides).map(routeItem => (
-                        <Route
-                          key={routeItem}
-                          path={`/${routeItem.toLowerCase()}`}
-                          component={Guides[routeItem]}
-                        />
-                      ))}
-                      <Route path="/" component={Contribute} />
-                    </Switch>
-                  </main>
-                </section>
-              </section>
-            )
-          }}
-        />
-      </BrowserRouter>
-    )
-  }
+  return (
+    <HashRouter basename="/design-system">
+      <section className={styles.App}>
+        <header className={styles.AppHeader}>
+          <Link className={styles.Home} to="/">
+            <i className={`fa fa-home ${styles.link}`} />{' '}
+          </Link>
+          <h1>Zesty.io Component Library</h1>
+        </header>
+        <section className={styles.AppWrap}>
+          <nav className={styles.AppMenu}>
+            <div className={styles.Title}>ATOMS</div>
+            <Nav
+              selected={selected}
+              tree={props.atoms.map(el => {
+                return {
+                  label: el,
+                  path: getPath(el),
+                  icon: 'cube'
+                }
+              })}
+            />
+            <div className={styles.Title}>MOLECULES</div>
+            <Nav
+              selected={selected}
+              tree={props.molecules.map(el => {
+                return {
+                  label: el,
+                  path: getPath(el),
+                  icon: 'cogs'
+                }
+              })}
+            />
+            <div className={styles.Title}>ORGANISMS</div>
+            <Nav
+              selected={selected}
+              tree={props.organisms.map(el => {
+                return {
+                  label: el,
+                  path: getPath(el),
+                  icon: 'fa-snowflake-o'
+                }
+              })}
+            />
+          </nav>
+          <main className={styles.AppShowcase}>
+            <Switch>
+              {/* Render routes for each component */}
+              {Object.keys(Guides).map(routeItem => (
+                <Route
+                  key={routeItem}
+                  path={`/design-system#/${routeItem.toLowerCase()}`}
+                  component={Guides[routeItem]}
+                />
+              ))}
+              <Route path="/" component={Contribute} />
+            </Switch>
+          </main>
+        </section>
+      </section>
+    </HashRouter>
+  )
 }
