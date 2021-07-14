88dd9ea0e5e7a064a8ea03d8631117a6c33d7ea2
ardeay <apuzzo@gmail.com>
2021-07-14 13:42:32 -0700

    new Docs component

---
+++ core/src/Docs/Docs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Docs.less";
+import cx from "classnames";
+
+export function Docs(props) {
+  let opts = {
+    className: cx(styles.ButtonGroup, props.className)
+  };
+  return (
+    <div {...opts}>
+      aszdfsadf
+    </div>
+  );
+}
