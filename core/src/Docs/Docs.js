0c8a1a5c8c2d00d33a7757691d49787429c0e25e
ardeay <apuzzo@gmail.com>
2021-07-15 13:39:53 -0700

    refactor to url the URL component over button

--- core/src/Docs/Docs.js
+++ core/src/Docs/Docs.js
@@ -2,22 +2,15 @@ import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBook } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "../Button"
+import { Url } from "../Url"
+import styles from "./Docs.less";
 
 export function Docs(props) {
 
-  function openDocs() {
-    let url=`https://zesty.org/?q=${props.subject}`
-    if(props.url){
-      url = props.url
-    }
-    window.open(url, '_blank');
-  }
-
   return (
-    <Button title={`Open documentation for ${props.subject}`} size="small" kind="documentation" onClick={openDocs}>
-      <FontAwesomeIcon icon={faBook} />
+    <Url title={`Open documentation for ${props.subject}`} className={styles.DocsLink} href={props.url ? props.url : `https://zesty.org/?q=${props.subject}`}>
+      <FontAwesomeIcon icon={faBook} className={styles.icon} />
         Documentation
-    </Button>
+    </Url>
   );
 }
