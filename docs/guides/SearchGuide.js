64b293c4b10b7b104499353fb7bcffd07af948d7
David Naimi <d88naimi@gmail.com>
2021-07-19 11:55:14 -0700

    removing commented out code

--- docs/guides/SearchGuide.js
+++ docs/guides/SearchGuide.js
@@ -18,13 +18,13 @@ export class SearchGuide extends Component {
         <br />
         <p>Rounded Search Bar</p>
         <p>
-          Props: onClick, onKeyup, override, noButton, roundedSearch: "true"{" "}
+          Props: onClick, onKeyup, override, noButton, roundedEdge: "true"{" "}
         </p>
         <Search
           placeholder="Search for something"
           onSubmit={console.log}
           onKeyUp={console.log}
-          roundedSearch="true"
+          roundedEdge="true"
         />
         <br />
         <CollapsibleCard header="Usage" open>
@@ -36,7 +36,7 @@ export class SearchGuide extends Component {
     placeholder="Search for something"
     onSubmit={this.onSearch}
     onKeyUp={this.onSearch}
-    roundedSearch: "true"
+    roundedEdge: "true"
   />`}
           />
         </CollapsibleCard>
