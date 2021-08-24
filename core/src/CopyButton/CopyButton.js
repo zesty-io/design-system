f9a59440241a4bc9542f528404eda2cdd590b7ed
David Naimi <d88naimi@gmail.com>
2021-08-24 13:06:44 -0700

    Feature quick copy button to help user's workflow (#147)
    * adding copy chip component WIP for docs
    
    * swapped hex color to variable
    
    * moved icon to left and utilizing small button component now
    
    * updating spacing on Copied text
    
    * added props classname to allow for styles overwrite
    
    * adding CopyChip docs and example
    
    * review updates
    
    * review pr styles updates and clean up
    
    * convert copyChip to copyButton removed border radius, clean up styles
    
    * removed span and moved onClick onto button
    
    * update docs removed passing props

---
+++ core/src/CopyButton/CopyButton.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import cx from "classnames";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboard, faCheck } from "@fortawesome/free-solid-svg-icons";
+
+import { Button } from "../Button";
+
+import styles from "./CopyButton.less";
+
+export const CopyButton = (props) => {
+  const [copied, setCopied] = useState(false);
+
+  function fallback(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+      // IE specific code path to prevent textarea being shown while dialog is visible.
+      return window.clipboardData.setData("Text", text);
+    } else if (
+      document.queryCommandSupported &&
+      document.queryCommandSupported("copy")
+    ) {
+      const textarea = document.createElement("textarea");
+      textarea.innerText = text;
+      document.body.appendChild(textarea);
+      textarea.select();
+      try {
+        setCopied(true);
+        document.execCommand("copy");
+      } catch (err) {
+        console.err(err);
+        return false;
+      } finally {
+        document.body.removeChild(textarea);
+      }
+    }
+  }
+
+  const copyValue = () => {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(props.value);
+      setCopied(true);
+    } else {
+      fallback(props.value);
+    }
+  };
+
+  useEffect(() => {
+    let iconTimer = setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(iconTimer);
+    };
+  }, [copied]);
+
+  return (
+      <Button className={cx(styles.CopyButton, props.className)} onClick={copyValue} kind="outlined" size="small">
+        {copied ? (
+          <FontAwesomeIcon
+            className={cx(styles.Icon, styles.CheckIcon)}
+            icon={faCheck}
+          />
+        ) : (
+          <FontAwesomeIcon className={styles.Icon} icon={faClipboard} />
+        )}
+        {props.children ? props.children : props.value}
+      </Button>
+  );
+};
---
+++ docs/guides/CopyButtonGuide.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+import { CopyButton } from "@zesty-io/core/CopyButton";
+
+import GithubEmbed from "../components/githubembed";
+import { CollapsibleCard } from "@zesty-io/core/CollapsibleCard";
+import { Card, CardHeader, CardContent, CardFooter } from "@zesty-io/core/Card";
+
+
+export class CopyButtonGuide extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div id="Guide">
+          <div id="Description">
+            <h1>Copy Chip Component</h1>
+            <p>A flexible copy button that can copy text or text fields</p>
+          </div>
+        </div>
+
+        <div id="Examples">
+          <Card>
+            <CardHeader>
+              <h2>How it looks</h2>
+            </CardHeader>
+            <CardContent>
+              <CopyButton value={"Hello Zesty"}></CopyButton>
+              <br />
+            </CardContent>
+          </Card>
+        </div>
+
+        <CollapsibleCard header="Usage" open>
+          <GithubEmbed
+            height="50px"
+            code="
+            <CopyButton value={props.example}} />"
+          />
+        </CollapsibleCard>
+        <CollapsibleCard collapsed header="Code">
+          <GithubEmbed
+            height="750px"
+            url="https://gist.githubusercontent.com/d88naimi/d5512f14f207a4e7bbeb9d2af48a04ac/raw/bc3177ba9d2d6464703d50081b3e4e835a929e9e/CopyChip.js"
+          />
+        </CollapsibleCard>
+      </React.Fragment>
+    );
+  }
+}
