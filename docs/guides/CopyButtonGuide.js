fff1143417d6a0a5bafc4d550b7e2ff3cbf68848
David Naimi <d88naimi@gmail.com>
2021-08-24 15:39:32 -0700

    updating docs

--- docs/guides/CopyButtonGuide.js
+++ docs/guides/CopyButtonGuide.js
@@ -13,7 +13,7 @@ export class CopyButtonGuide extends Component {
       <React.Fragment>
         <div id="Guide">
           <div id="Description">
-            <h1>Copy Chip Component</h1>
+            <h1>Copy Button Component</h1>
             <p>A flexible copy button that can copy text or text fields</p>
           </div>
         </div>
@@ -40,7 +40,7 @@ export class CopyButtonGuide extends Component {
         <CollapsibleCard collapsed header="Code">
           <GithubEmbed
             height="750px"
-            url="https://gist.githubusercontent.com/d88naimi/d5512f14f207a4e7bbeb9d2af48a04ac/raw/bc3177ba9d2d6464703d50081b3e4e835a929e9e/CopyChip.js"
+            url="https://gist.githubusercontent.com/d88naimi/5974ba306e5756704b6f73e07302054c/raw/936877280ce9f7dc36d5b54eb78192b9880b4cb1/CopyButton.js"
           />
         </CollapsibleCard>
       </React.Fragment>
